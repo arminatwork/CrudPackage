a2b581c2365eefad5821c9b3e76f3eab4fcca71e
arminatwork <workwitharmin7@gmail.com>
2021-02-10 20:25:50 +0330

    Setup Structure Project

---
+++ src/web/api/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        //have your code here
+    }
+}
