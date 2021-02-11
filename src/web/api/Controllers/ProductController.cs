2d23b93b84f2d9613d89a3b1d74a3ba8d29ccf06
arminatwork <workwitharmin7@gmail.com>
2021-02-11 19:27:39 +0330

    Create BaseRepository with BaseContext

--- src/web/api/Controllers/ProductController.cs
+++ src/web/api/Controllers/ProductController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using api.Data.Repositories;
+using api.Models.Product;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
@@ -6,6 +10,59 @@ namespace api.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        //have your code here
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ActionResult<IEnumerable<Product>> GetProduct()
+        {
+            return _repository.GetByList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var entity = _repository.GetById(id);
+            if (entity is null) return NotFound($"Entity {id} not found! ...");
+
+            return entity;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> AddProduct(Product product)
+        {
+            _repository.Add(product);
+            return Ok(product);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<Product> UpdateProduct(int id, Product product)
+        {
+            var entity = _repository.GetById(id);
+            if (entity is null) return NotFound($"Entity {id} not found! ...");
+
+            entity.Name = product.Name ?? entity.Name;
+            entity.Description = product.Description ?? entity.Description;
+            entity.Price = product.Price;
+            entity.UpdatedAt = DateTimeOffset.Now;
+
+            _repository.Update(entity);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Product> DeleteProduct(int id)
+        {
+            var entity = _repository.GetById(id);
+            if (entity is null) return NotFound($"Entity {id} not found! ...");
+
+            _repository.Delete(entity);
+
+            return NoContent();
+        }
     }
 }
--- src/web/api/Startup.cs
+++ src/web/api/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+using api.Data.Context;
+using api.Data.Repositories;
 
 namespace api
 {
@@ -18,8 +22,14 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllers();
+
+            // using Microsoft.EntityFrameworkCore;
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
