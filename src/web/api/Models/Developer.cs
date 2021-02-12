982a798b1adbb9e8ac26dfa85a52c64213988b23
arminatwork <workwitharmin7@gmail.com>
2021-02-12 19:19:27 +0330

    create Developer model

---
+++ src/web/api/Models/Developer.cs
@@ -0,0 +1,15 @@
+using api.Data.BaseEntity;
+
+namespace api.Models
+{
+    public class Developer : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Follower { get; set; }
+
+
+    }
+}
