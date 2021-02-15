0267aa9d31aa4f204d4b1c096ded2a31b399a416
arminatwork <workwitharmin7@gmail.com>
2021-02-15 13:15:28 +0330

    Add Asynchronous Method to Use as Async

--- src/lib/Crud/BaseRepository/BaseRepository.cs
+++ src/lib/Crud/BaseRepository/BaseRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -33,7 +34,10 @@ namespace Crud.BaseRepository
         {
             return Entities.Find(ids);
         }
-
+        public virtual ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids)
+        {
+            return Entities.FindAsync(ids, cancellationToken);
+        }
         public virtual void Add(TEntity entity, bool saveNow = true)
         {
             Assert.NotNull(entity, nameof(entity));
@@ -41,20 +45,12 @@ namespace Crud.BaseRepository
             if (saveNow)
                 DbContext.SaveChanges();
         }
-
-        public virtual void Update(TEntity entity, bool saveNow = true)
+        public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
         {
             Assert.NotNull(entity, nameof(entity));
-            Entities.Update(entity);
+            await Entities.AddAsync(entity, cancellationToken).ConfigureAwait(false);
             if (saveNow)
-                DbContext.SaveChanges();
-        }
-        public virtual void Delete(TEntity entity, bool saveNow = true)
-        {
-            Assert.NotNull(entity, nameof(entity));
-            Entities.Remove(entity);
-            if (saveNow)
-                DbContext.SaveChanges();
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
         }
 
         public virtual void AddRange(IEnumerable<TEntity> entities, bool saveNow = true)
@@ -65,6 +61,28 @@ namespace Crud.BaseRepository
                 DbContext.SaveChanges();
 
         }
+        public virtual async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entities, nameof(entities));
+            await Entities.AddRangeAsync(entities, cancellationToken).ConfigureAwait(false);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+        public virtual void Update(TEntity entity, bool saveNow = true)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Update(entity);
+            if (saveNow)
+                DbContext.SaveChanges();
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Update(entity);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
 
         public virtual void UpdateRange(IEnumerable<TEntity> entities, bool saveNow = true)
         {
@@ -73,13 +91,42 @@ namespace Crud.BaseRepository
             if (saveNow)
                 DbContext.SaveChanges();
         }
-
+        public virtual async Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entities, nameof(entities));
+            Entities.UpdateRange(entities);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+        public virtual void Delete(TEntity entity, bool saveNow = true)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.Remove(entity);
+            if (saveNow)
+                DbContext.SaveChanges();
+        }
         public virtual void DeleteRange(IEnumerable<TEntity> entities, bool saveNow = true)
         {
             Assert.NotNull(entities, nameof(entities));
             Entities.RemoveRange(entities);
             if (saveNow)
                 DbContext.SaveChanges();
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entity, nameof(entity));
+            Entities.RemoveRange(entity);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public virtual async Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            Assert.NotNull(entities, nameof(entities));
+            Entities.RemoveRange(entities);
+            if (saveNow)
+                await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
         }
     }
 }
--- src/lib/Crud/BaseRepository/IBaseRepository.cs
+++ src/lib/Crud/BaseRepository/IBaseRepository.cs
@@ -1,17 +1,35 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Crud.BaseRepository
 {
     public interface IBaseRepository<TEntity> where TEntity : class
     {
+        DbSet<TEntity> Entities { get; }
+        IQueryable<TEntity> Table { get; }
+        IQueryable<TEntity> TableNoTracking { get; }
 
         List<TEntity> GetByList();
         TEntity GetById(params object[] ids);
+        ValueTask<TEntity> GetByIdAsync(CancellationToken cancellationToken, params object[] ids);
+
         void Add(TEntity entity, bool saveNow = true);
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
         void AddRange(IEnumerable<TEntity> entities, bool saveNow = true);
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
+
         void Update(TEntity entity, bool saveNow = true);
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
         void UpdateRange(IEnumerable<TEntity> entities, bool saveNow = true);
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
+
         void Delete(TEntity entity, bool saveNow = true);
         void DeleteRange(IEnumerable<TEntity> entities, bool saveNow = true);
+        Task DeleteAsync(TEntity entity, CancellationToken cancellationToken, bool saveNow = true);
+        Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken, bool saveNow = true);
     }
 }
