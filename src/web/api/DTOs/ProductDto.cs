d680e12d7e641bc41a6247981c3eb926eef7c82a
arminatwork <workwitharmin7@gmail.com>
2021-02-16 15:02:49 +0330

    Insert Sample Dto Class

---
+++ src/web/api/DTOs/ProductDto.cs
@@ -0,0 +1,19 @@
+using api.Models.Product;
+
+using Crud.BaseDTO;
+
+namespace api.DTOs
+{
+    public class ProductDto : BaseDto<ProductDto, Product, int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class ProductSelectDto : BaseDto<ProductSelectDto, Product, int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
