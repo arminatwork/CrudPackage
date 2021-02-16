4eed9a689dd6a6670128788a262947db821e39c4
arminatwork <workwitharmin7@gmail.com>
2021-02-16 19:11:53 +0330

    Fix Bug

--- src/web/api/DTOs/ProductDto.cs
+++ src/web/api/DTOs/ProductDto.cs
@@ -1,3 +1,4 @@
+using System;
 using api.Models.Product;
 
 using Crud.BaseDTO;
@@ -9,11 +10,16 @@ namespace api.DTOs
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+
     }
     public class ProductSelectDto : BaseDto<ProductSelectDto, Product, int>
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+         public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
     }
 }
